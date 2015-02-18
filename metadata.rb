0ef4aa99e62377ec91d7e78427c06fa368a907ca
Mahesh Varma <Maheshkumar.Varma@dowjones.com>
2015-02-18 13:58:25 -0500

    structural changes

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "line"
+maintainer       "Mahesh Varma"
+maintainer_email "varmamahesh@yahoo.com"
+license          "Apache2"
+description      "Provides line editing resources for use by recipes"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.5.1"
+
